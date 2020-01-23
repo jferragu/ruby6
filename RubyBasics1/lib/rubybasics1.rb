764f3434ae778fefeec5993d01ff5f8b204bc474
Jacob Ferragut <jferragu@uncc.edu>
2020-01-23 02:08:47 +0000

    RubyBasics1 - Part 3

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -11,5 +11,17 @@ end
 
 # Part III
 def sum_to_n? arr, n
-  return arr.max(n).sum
+  if arr.length()<=1
+    return false
+  end
+  
+  for i in 0..arr.length()
+    for j in i+1..arr.length()-1
+      if (arr[i]+arr[j])==n
+        return true
+      end
+    end
+  end
+  
+  return false
 end
