963e7703dda6d49f8163397b0825823e0a6edef7
Jacob Ferragut <jferragu@uncc.edu>
2020-01-23 01:47:56 +0000

    RubyBasics1 - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,15 @@
+# Lab 1
+# Part I
+def sum arr
+  return arr.sum
+end
+
+# Part II
+def max_2_sum arr
+  return arr.max(2).sum
+end
+
+# Part III
+def sum_to_n? arr, n
+  return arr.max(n).sum
+end
